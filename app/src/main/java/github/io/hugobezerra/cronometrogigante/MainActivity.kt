5c1307ec0131e11b6f9992347d8786ae8de93cc2
Hugo de Araujo Bezerra <hugoinfor@gmail.com>
2025-10-08 23:18:52 -0300

    Inicial commit

---
+++ app/src/main/java/github/io/hugobezerra/cronometrogigante/MainActivity.kt
@@ -0,0 +1,75 @@
+package github.io.hugobezerra.cronometrogigante
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvTimer: TextView
+    private var timer = 0L
+    private var isRunning = false
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        tvTimer = findViewById(R.id.tvTimer)
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnStop = findViewById<Button>(R.id.btnStop)
+        val btnReset = findViewById<Button>(R.id.btnReset)
+
+        handler = Handler(Looper.getMainLooper())
+
+        btnStart.setOnClickListener {
+            if (!isRunning) {
+                isRunning = true
+                startTimer()
+            }
+        }
+
+        btnStop.setOnClickListener {
+            isRunning = false
+        }
+
+        btnReset.setOnClickListener {
+            isRunning = false
+            timer = 0
+            updateTimerText()
+        }
+    }
+
+    private fun startTimer() {
+        val runnable = object : Runnable {
+            override fun run() {
+                if (isRunning) {
+                    timer++
+                    updateTimerText()
+                    handler.postDelayed(this, 1000)
+                }
+            }
+        }
+        handler.post(runnable)
+    }
+
+    private fun updateTimerText() {
+        val hours = (timer / 3600)
+        val minutes = (timer % 3600) / 60
+        val seconds = (timer % 60)
+        tvTimer.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    }
+}
